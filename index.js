78403de54b6bf24519830d107f777a232e6b1a45
lohith yadav <viperlohith121@gmail.com>
2022-08-26 12:13:33 +0530

    samlple 1

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res, next) => {
-    res.send("It's working")
+    res.send("It's working awesome")
 })
 
 app.listen(PORT, () => {
