95aa067282a41fec3b2bb3e956b68ddbe6b76288
lohith yadav <viperlohith121@gmail.com>
2022-08-29 14:50:43 +0530

    added data route

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ app.get("/", (req, res, next) => {
     res.send("It's working awesome")
 })
 
+app.get('/data', (req, res, next) => {
+    res.send([
+        {
+          id: 1,
+          name: "john"
+        },
+        {
+            id: 2,
+            name: "smitch"
+        }
+    ])
+})
+
 app.listen(PORT, () => {
     console.log(`App liesting at ${PORT}`)
 })
